fb021adfb9fb3bf5c1772fea802b683a546c7789
Zohaib-farooq-dev <zohaibf595@gmail.com>
2025-08-27 13:51:21 +0500

    learn about query parameters and implement it

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 
@@ -33,7 +33,18 @@ def view_patient(patient_id:str = Path(...,description='ID of the patient in DB'
         return data[patient_id]
     raise HTTPException(status_code=404, detail='Patient not Found!')
 
-
+@app.get('/sort')
+def sorted_patients(sort_by:str = Query('weight', description='Sort on the basis of wirght ,hight and bmi'), order:str= Query('asc',description="Sosrt values in ascending or descending order")):
+    valid_fields =['height','weight','bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code='400',detail=f'Invalid fields select from {valid_fields}')
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code='400',detail='Invalid order select asc or desc')
+    
+    data = load_data()
+    sort_order = True if order =='desc' else False
+    sort = sorted(data.values(),key= lambda x:x.get(sort_by,0),reverse=sort_order)
+    return sort
 
 
 
